15fdedb9ae57188d40d202e9bf1d916da82a9b01
wneoh <weichin092@gmail.com>
2020-05-06 19:01:07 -0500

    created login page

--- controllers/main.js
+++ controllers/main.js
@@ -6,7 +6,7 @@ exports.getIndex =(req,res,next)=>{
         //console.log(products);
         res.render('Index',{
         title:"Home",
-        slide_hero:["/images/apple.jpg","/images/controller.jpg","/images/apple1.jpg","/images/mobile.jpg"],
+        slide_hero:["/images/home/apple-home.jpg","/images/home/controller-home.jpg","/images/home/camera-home.jpg","/images/home/phone-home.jpg"],
         products:products,
         csrfToken: req.csrfToken()
         })
